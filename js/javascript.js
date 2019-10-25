920e232ca71b02b5a07b85244c314db99332d960
Matias <mgbrunini@gmail.com>
2019-10-25 00:01:00 -0300

    Segunda fila completa

--- js/javascript.js
+++ js/javascript.js
@@ -8,4 +8,12 @@ $(document).ready(function(){
   $("#negro7").position({my: "left top",at: "right top",of: "#blanco6"});
   $("#blanco8").position({my: "left top",at: "right top",of: "#negro7"});
   //fila 2
+  $("#blanco1").position({my: "top",at: "bottom",of: "#negro1"});
+  $("#negro2").position({my: "top",at: "bottom",of: "#blanco2"});
+  $("#blanco3").position({my: "top",at: "bottom",of: "#negro3"});
+  $("#negro4").position({my: "top",at: "bottom",of: "#blanco4"});
+  $("#blanco5").position({my: "top",at: "bottom",of: "#negro5"});
+  $("#negro6").position({my: "top",at: "bottom",of: "#blanco6"});
+  $("#blanco7").position({my: "top",at: "bottom",of: "#negro7"});
+  $("#negro8").position({my: "top",at: "bottom",of: "#blanco8"});
 })
