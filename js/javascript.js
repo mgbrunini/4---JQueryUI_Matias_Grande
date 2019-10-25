0c79c812303202d9eb54e4a35c2920d35ae47cb2
Matias <mgbrunini@gmail.com>
2019-10-24 23:47:01 -0300

    Primera linea completa

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-  
+  //Fila 1
+  $("#blanco2").position({my: "left top",at: "right top",of: "#negro1"});
+  $("#negro3").position({my: "left top",at: "right top",of: "#blanco2"});
+  $("#blanco4").position({my: "left top",at: "right top",of: "#negro3"});
+  $("#negro5").position({my: "left top",at: "right top",of: "#blanco4"});
+  $("#blanco6").position({my: "left top",at: "right top",of: "#negro5"});
+  $("#negro7").position({my: "left top",at: "right top",of: "#blanco6"});
+  $("#blanco8").position({my: "left top",at: "right top",of: "#negro7"});
+  //fila 2
 })
