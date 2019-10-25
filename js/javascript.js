f9830ce23589e0c09871dff634ea4a0b563f3388
Matias <mgbrunini@gmail.com>
2019-10-25 01:22:25 -0300

    Varias filas realizadas

--- js/javascript.js
+++ js/javascript.js
@@ -16,4 +16,40 @@ $(document).ready(function(){
   $("#negro6").position({my: "top",at: "bottom",of: "#blanco6"});
   $("#blanco7").position({my: "top",at: "bottom",of: "#negro7"});
   $("#negro8").position({my: "top",at: "bottom",of: "#blanco8"});
+  //fila 3
+  $("#negro31").position({my: "top",at: "bottom",of: "#blanco1"});
+  $("#blanco32").position({my: "top",at: "bottom",of: "#negro2"});
+  $("#negro33").position({my: "top",at: "bottom",of: "#blanco3"});
+  $("#blanco34").position({my: "top",at: "bottom",of: "#negro4"});
+  $("#negro35").position({my: "top",at: "bottom",of: "#blanco5"});
+  $("#blanco36").position({my: "top",at: "bottom",of: "#negro6"});
+  $("#negro37").position({my: "top",at: "bottom",of: "#blanco7"});
+  $("#blanco38").position({my: "top",at: "bottom",of: "#negro8"});
+  //Fila 4
+  $("#blanco41").position({my: "top",at: "bottom",of: "#negro31"});
+  $("#negro42").position({my: "top",at: "bottom",of: "#blanco32"});
+  $("#blanco43").position({my: "top",at: "bottom",of: "#negro33"});
+  $("#negro44").position({my: "top",at: "bottom",of: "#blanco34"});
+  $("#blanco45").position({my: "top",at: "bottom",of: "#negro35"});
+  $("#negro46").position({my: "top",at: "bottom",of: "#blanco36"});
+  $("#blanco47").position({my: "top",at: "bottom",of: "#negro37"});
+  $("#negro48").position({my: "top",at: "bottom",of: "#blanco38"});
+  //fila 5
+  $("#negro51").position({my: "top",at: "bottom",of: "#blanco41"});
+  $("#blanco52").position({my: "top",at: "bottom",of: "#negro42"});
+  $("#negro53").position({my: "top",at: "bottom",of: "#blanco43"});
+  $("#blanco54").position({my: "top",at: "bottom",of: "#negro44"});
+  $("#negro55").position({my: "top",at: "bottom",of: "#blanco45"});
+  $("#blanco56").position({my: "top",at: "bottom",of: "#negro46"});
+  $("#negro57").position({my: "top",at: "bottom",of: "#blanco47"});
+  $("#blanco58").position({my: "top",at: "bottom",of: "#negro48"});
+  //fila6
+  $("#blanco61").position({my: "top",at: "bottom",of: "#negro51"});
+  $("#negro62").position({my: "top",at: "bottom",of: "#blanco52"});
+  $("#blanco63").position({my: "top",at: "bottom",of: "#negro53"});
+  $("#negro64").position({my: "top",at: "bottom",of: "#blanco54"});
+  $("#blanco65").position({my: "top",at: "bottom",of: "#negro55"});
+  $("#negro66").position({my: "top",at: "bottom",of: "#blanco56"});
+  $("#blanco67").position({my: "top",at: "bottom",of: "#negro57"});
+  $("#negro68").position({my: "top",at: "bottom",of: "#blanco58"});
 })
