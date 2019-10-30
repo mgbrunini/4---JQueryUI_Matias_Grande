fd92386276ee70c03bdf3566133bf3ecf7261698
Matias <mgbrunini@gmail.com>
2019-10-29 23:57:01 -0300

    Codificacion del script

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,24 @@
 $(document).ready(function(){
-  $(".fichaRoja").draggable();
-  $(".fichaNegra").draggable();
+  $(".fichaRoja").draggable({
+    revert: 'invalid'
+  });
+  $(".fichaNegra").draggable({
+    revert: 'invalid'
+  });
+  $(".negro").droppable({
+    accept: function (element) {
+      if (element.hasClass("fichaRoja") || element.hasClass("fichaNegra")) {
+        return false;
+      }
+      return true;
+    }
+  });
+  $(".blanco").droppable({
+    accept: function (element) {
+      if (element.hasClass("fichaRoja") || element.hasClass("fichaNegra")) {
+        return true;
+      }
+      return false;
+    }
+  });
 })
